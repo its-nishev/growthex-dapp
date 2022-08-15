2fd225c541e6d8899cb9ad919af3ace175ba19b6
Nishev <iamnishev@gmail.com>
2022-08-15 13:38:28 +0530

    Create Context

---
+++ context/robinhoodContext.js
@@ -0,0 +1,61 @@
+import { createContext, useEffect, useState } from 'react'
+import { useMoralis } from 'react-moralis'
+
+export const RobinhoodContext = createContext()
+
+export const RobinhoodProvider = ({children}) => {
+
+
+  const [currentAccount, setCurrentAccount] = useState('')
+  const [formattedAccount, setFormattedAccount] = useState('')
+
+  const { isAuthenticated, authenticate, user, Logout, Moralis, enableWeb3 } = useMoralis()
+
+  useEffect(() => {
+    if(isAuthenticated) {
+      const account = user.get('ethAddress')
+      let formattedAccount = account.slice(0,4)+'...'+account.slice(-4)
+      setFormattedAccount(formattedAccount)
+    }
+  },[isAuthenticated, enableWeb3])
+
+  useEffect(() => {
+    if (!currentAccount) return
+    ;(async () => {
+      const response = await fetch('/api/createUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          walletAddress: currentAccount,
+        }),
+      })
+
+      const data = await response.json()
+    })()
+  }, [currentAccount])
+
+  const ConnectWallet = () => {
+    authenticate()
+  }
+
+  const signOut = () => {
+    Logout()
+  }
+
+
+  return (
+    <RobinhoodContext.Provider
+      value = {{
+        ConnectWallet,
+        signOut,
+        currentAccount,
+        isAuthenticated,
+        formattedAccount
+      }}
+    >
+      {children}
+    </RobinhoodContext.Provider>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { RobinhoodProvider } from '../context/robinhoodContext'
+import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl='https://1v8rrkivyd3p.usemoralis.com:2053/server'
+      appId='AY3r7WBGbIfqtjx4HmaKPYcWviGoEfAoY1CPnxNY'
+    >
+      <RobinhoodProvider>
+        <Component {...pageProps} />
+      </RobinhoodProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
